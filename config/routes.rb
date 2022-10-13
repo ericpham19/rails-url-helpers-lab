a60620cee41c97a09665af7d30ba9875f4463a03
Duy Pham <ericpham19@outlook.com>
2022-10-13 20:53:19 +1100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get "students", to: "students#index"
     get "/students/:id", to: "students#show", as: "student"
-end
+    get "/students/:id/activate", to: "students#activate" , as: "activate_student"
+ end
